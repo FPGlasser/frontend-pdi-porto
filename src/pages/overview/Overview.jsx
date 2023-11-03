998ee9a34b8701418bc2256903eadfec4ab7d614
JustinoCarneiro <140731523+JustinoCarneiro@users.noreply.github.com>
2023-11-03 01:38:38 -0300

    Update Overview.jsx

--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
-import style from './style.module.css'
+import styles from './Overview.module.css'
 
 const Overview = () => {
   return (
-    <div>Overview</div>
+    <div className={styles.body}>
+      <div className={styles.container}>
+        <h3 className={styles.data}>23/12/2024</h3>
+        <div className={styles.line}></div>
+        <h3 className={styles.horario}>04:40</h3>
+        <div className={styles.line}></div>
+        <h3 className={styles.sucesso}>SUCESSO</h3>
+      </div>
+      <div className={styles.container}>
+        <h3 className={styles.data}>05/10/2024</h3>
+        <div className={styles.line}></div>
+        <h3 className={styles.horario}>14:34</h3>
+        <div className={styles.line}></div>
+        <h3 className={styles.fracasso}>FRACASSO</h3>
+      </div>
+      <div className={styles.container}>
+        <h3 className={styles.data}>02/05/2024</h3>
+        <div className={styles.line}></div>
+        <h3 className={styles.horario}>03:49</h3>
+        <div className={styles.line}></div>
+        <h3 className={styles.sucesso}>SUCESSO</h3>
+      </div>
+    </div>
   )
 }
 
