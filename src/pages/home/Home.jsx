6c8f3d3ea5b9a5a2e7e0fe5934c087691e8cf24e
JustinoCarneiro <140731523+JustinoCarneiro@users.noreply.github.com>
2023-11-03 01:37:14 -0300

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-import style from './style.module.css'
+import styles from './Home.module.css'
+import dashboard from '../../data/dashboard.json'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+    {dashboard.map((item) => (
+      <div className={styles.body}>
+        <div className={styles.container_menor}>
+          <h1>Ângulo</h1>
+          <p>{item.angulo} °</p>
+        </div>
+        <div className={styles.velocidade}>
+          <h1>Velocidade</h1>
+          <p>{item.velocidade} cm/s</p>
+        </div>
+        <div className={styles.container_maior}>
+          <h2>Distância direita</h2>
+          <p>{item.distancia_d} m</p>
+          <h2>Distância esquerda</h2>
+          <p>{item.distancia_e} m</p>
+        </div>
+      </div>
+      ))}
+    </div>
   )
 }
 
